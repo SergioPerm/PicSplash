358bc216bd01127538ff405790d5c9cde2902a21
Sergio Lechini <itotdel@list.ru>
2021-08-23 18:53:41 +0500

    project init

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicSplash
+//
+//  Created by Sergio Lechini on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
